2f657ef001b1ac2fd159dff3c1bdc4db9799c2f0
saferprotocol <safermoonprotocol@gmail.com>
2021-06-13 16:15:45 -0400

    modified deployment; added to .gitignore; simplified truffle-config.js

--- deploy-application.sh
+++ deploy-application.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-echo "Cleaning up work space"
-rm -rf build
-rm -rf node_modules
+logDirectory="./logs/"
+mkdir -p $logDirectory 
+logFile="$logDirectory$(date +"%Y_%m_%d_%I_%M_%p").log"
+echo "Piping output to $logFile"
 
-echo "Building work space"
-npm install
-node ./automation/create-env.js
-# node ./automation/echo-account-address.js
-echo "Fetch Account Address and Populate Liquidity Pool; Send Ether."
-read -p "Press enter to continue to smart contract build & deployment."
-
-
-echo "Building smart contract."
-truffle version
-truffle compile
-
-echo "Deploying smart contract."
-truffle deploy --network ropsten
+./_deploy-app.sh "\n" > $logFile 2>&1
+cat $logFile
 read -p "Press enter to close window ..."
